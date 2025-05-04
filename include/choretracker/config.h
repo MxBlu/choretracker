69143d4febc7a52809fb4de23a19ee90a35557b7
Jumail Mundekkat <mundekkat@hotmail.com>
2025-05-04 22:45:43 +1000

    Add simple discord client configuration

--- include/choretracker/config.h
+++ include/choretracker/config.h
@@ -4,6 +4,7 @@
 #include <string>
 
 #define CONFIG_BOT_TOKEN "bot_token"
+#define CONFIG_TEST_GUILD "test_guild"
 
 bool config_load_file();
 std::optional<std::string> config_get_str(std::string property);
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,27 @@
 
 #include "choretracker/config.h"
 
+void list_chores(const dpp::slashcommand_t &event) {
+    auto user_id = event.command.usr.id;
+    auto guild_id = event.command.guild_id;
+
+    event.reply(dpp::message("Received!").set_flags(dpp::m_ephemeral));
+}
+
+void add_chore(const dpp::slashcommand_t &event) {
+    auto user_id = event.command.usr.id;
+    auto guild_id = event.command.guild_id;
+
+    event.reply(dpp::message("Received!").set_flags(dpp::m_ephemeral));
+}
+
+void delete_chore(const dpp::slashcommand_t &event) {
+    auto user_id = event.command.usr.id;
+    auto guild_id = event.command.guild_id;
+
+    event.reply(dpp::message("Received!").set_flags(dpp::m_ephemeral));
+}
+
 int main(int argc, char const *argv[]) {
     // Ensure config file is loaded
     bool config_was_loaded = config_load_file();
@@ -24,8 +45,41 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
-    // Initialise the bot and wait forever
     dpp::cluster bot(bot_token.value());
+
+    bot.on_ready([&bot](const dpp::ready_t &event) {
+        // Register commands
+        if (dpp::run_once<struct register_bot_commands>()) {
+            dpp::slashcommand list_chores_command("listchores", "List currently tracked chores", bot.me.id);
+
+            dpp::slashcommand add_chores_command("addchore", "Add a new chore", bot.me.id);
+            add_chores_command.add_option(
+                dpp::command_option(dpp::co_string, "name", "Name of chore", true));
+            add_chores_command.add_option(
+                dpp::command_option(dpp::co_integer, "frequency", "Frequency in days", true));
+            
+            dpp::slashcommand delete_chores_command("deletechore", "Delete a chore", bot.me.id);
+            delete_chores_command.add_option(
+                dpp::command_option(dpp::co_string, "name", "Name of chore", true));
+
+            auto test_guild = config_get_str(CONFIG_TEST_GUILD).value();
+            bot.guild_bulk_command_create({
+                list_chores_command, add_chores_command, delete_chores_command
+            }, test_guild);
+        }
+    });
+
+    bot.on_slashcommand([](const dpp::slashcommand_t &event) {
+        if (event.command.get_command_name() == "listchores") {
+            list_chores(event);
+        } else if (event.command.get_command_name() == "addchore") {
+            add_chore(event);
+        } else if (event.command.get_command_name() == "deletechore") {
+            delete_chore(event);
+        }
+    });
+
+    // Initialise the bot and wait forever
     bot.start(dpp::st_wait);
 
     return 0;
