ab185b821093117ba33d9d352fe6978490987b1b
Jumail Mundekkat <mundekkat@hotmail.com>
2025-05-06 20:32:06 +1000

    Use vcpkg

--- cmake/FindSpdlog.cmake
+++
@@ -1,4 +0,0 @@
-find_path(SPDLOG_INCLUDE_DIR NAMES spdlog/spdlog.h HINTS ${SPDLOG_ROOT_DIR})
-find_library(SPDLOG_LIBRARY NAMES spdlog "libspdlog.a" HINTS ${SPDLOG_ROOT_DIR}) 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(spdlog DEFAULT_MSG SPDLOG_LIBRARY SPDLOG_INCLUDE_DIR)
